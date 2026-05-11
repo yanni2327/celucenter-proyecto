2a64163a70db053729b35a5eb1cc0d457fa70322
yanni2327 <dioyanniaranguren@gmail.com>
2026-05-10 21:35:48 -0500

    fix: corregir errores de sintaxis y rebuild

--- celucenter_web_build/flutter_bootstrap.js
+++ celucenter_web_build/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"59aa584fdf100e6c78c785d8a5b565d1de4b48
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "822718481" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "292620624" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
